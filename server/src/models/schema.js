25e1f49d2540a7a88c48c5b2c988a522e01dba94
Arushi Aggarwal <aggarwalarushi5@gmail.com>
2020-08-16 20:42:37 +0530

    Fixed Note Resolvers Pt1

--- server/src/models/schema.js
+++ server/src/models/schema.js
@@ -1,20 +1,18 @@
-const { buildSchema } = require("graphql");
-const note = require('./notes')
-const data = [{
-    id: 1,
-    title: "note 1",
-    content: "today was a good day",
-    image: "image!"
-},];
+const {
+    buildSchema
+} = require("graphql");
+const note = require('./notes');
+const users = require("./users");
+
 
 
 const schemaBuilt = buildSchema(`
 type Query{
     notes: [note]
-    Note(id: ID) : note
+    Note(_id: ID) : note
 }
 type note{
-    id : ID
+    _id : ID
     title : String
     content : String
     image : String
@@ -46,22 +44,53 @@ type Mutation {
 `);
 
 const resolver = {
-    createNote: async({input}) =>{
+    notes: async() => {
+        try {
+            const allNotes = await note.find((err,notes) =>{
+                if(err) return err
+                return notes
+            }) //? note is our collection here 
+            return allNotes
+        } catch (error) {
+            throw error
+        }
+    
+    },
+    Note : async({_id}) => {
+        try {
+            const oneNote = await note.findById({_id : _id})
+            return oneNote._doc
+        } catch (error) {
+            throw error
+        }
+    },
+    deleteNote : async({_id}) => {
+        try {
+            const delNote = await note.findByIdAndDelete({_id : _id})
+            return delNote
+        } catch (error) {
+            throw error
+        }
+    },
+    createNote: async ({
+        input
+    }) => {
         const Note = new note({
             title: input.title,
             content: input.content,
-            image: input.image
+            image: input.image,
         }) //need to include the user creator to link this note to one user
         let notes;
         try {
-        const result = await Note.save() //saved document
-        notes = {
-            ...result._doc// The spread syntax allows an expression to be expanded in places where multiple arguments are expected.
+            const result = await Note.save() //saved document
+            notes = {
+                ...result._doc
+                // The spread syntax allows an expression to be expanded in places where multiple arguments are expected.
+            }
+            return notes
+        } catch (error) {
+            throw error
         }
-    } catch (error) {
-        return Error(error)
-    }
-    return notes
     }
 };
 
